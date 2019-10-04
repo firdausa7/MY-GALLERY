6ffa0dba5f64bf32d6e5b3b945192b78b725e6de
firdausa7 <firdausa.salat@gmail.com>
2019-10-04 10:26:59 +0300

    run test for location model

--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+
+    #SetUp method  for Location
+    def setUp(self):
+        self.test_location = Location(name ='Nairobi')
+        self.test_location.save()
+
+        #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_location, Location))
