8e7efc747ed2d252336393b2959e5d05b53b0f95
firdausa7 <firdausa.salat@gmail.com>
2019-10-04 11:39:27 +0300

    add test for tags

--- photo/tests.py
+++ photo/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Test for Location
 class LocationTestClass(TestCase):
 
     #SetUp method  for Location
@@ -11,3 +13,9 @@ class LocationTestClass(TestCase):
         #Testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.test_location, Location))
+
+# Creating a new tag and saving it
+        self.new_tag = tags(name="testing")
+        self.new_tag.save()
+
+
