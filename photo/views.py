4c4138ae23b64984d5913982851e6728a90bc736
firdausa7 <firdausa.salat@gmail.com>
2019-10-06 17:12:02 +0300

    add image to homepage

--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.conf.urls import url,include
+from .models import Location, tags, Category, Image
 # Create your views here.
+
+
+#HOME PAGE VIEW FUNCTION
+#########################
+
 def index(request):
-    return render( request, 'home.html')
+    photos = Image.objects.all()
+    context = {'photos': photos}
+    return render( request, 'home.html', {'photos': photos})
+
