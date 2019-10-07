ce76794a47fab16437605fde41efbec16d99fcde
firdausa7 <firdausa.salat@gmail.com>
2019-10-07 08:26:56 +0300

    add search view fuction and url path

--- photo/urls.py
+++ photo/urls.py
@@ -6,5 +6,5 @@ urlpatterns=[
     url('^$', views.index,name='index'),
     url('location/', views.category, name='location'),
     url('category', views.category, name='category'),
-
+    url('search/', views.search_results, name='search_results'),
 ]
--- photo/views.py
+++ photo/views.py
@@ -23,3 +23,17 @@ def location(request):
 def category(request):
     return render(request,'category.html',)
 
+#SEARCH CAPABILITY Page View Function!
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_category = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"categorys": searched_category})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request,'search.html',{"message":message})
